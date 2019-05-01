37eee7cf8ca771b2f462b0075c5b358cc403342a
Claudio Orlandi <cso.junior1996@gmail.com>
2019-05-01 15:41:49 -0300

    Recriando pasta com nome corrigido

---
+++ src/pages/Main/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import {
+  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  fileName: {
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  instructions: {
+    color: '#DDD',
+    fontSize: 14,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  logo: {
+    height: Dimensions.get('window').height * 0.11,
+    marginVertical: Dimensions.get('window').height * 0.11,
+    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
+  },
+  welcome: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+const Main = () => (
+  <ImageBackground
+    source={{
+      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
+    }}
+    style={styles.container}
+    resizeMode="cover"
+  >
+    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+    <Image
+      source={{
+        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
+      }}
+      style={styles.logo}
+      resizeMode="contain"
+    />
+    <Text style={styles.welcome}>Bem-vindo ao Template Avançado!</Text>
+    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
+    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
+    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
+  </ImageBackground>
+);
+
+export default Main;
