8fda8cad2846d787dd1ab059a80de59f46fb41f4
rai.vieira <rai.vieira@bludata.com.br>
2019-05-14 12:49:55 -0300

    add package babel-plugin-transform-remove-console

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
