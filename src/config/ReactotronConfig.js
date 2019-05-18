26592af9f2866f0249054565b2a34ffbd9b15540
deciomontanhani <deciomontanhani@gmail.com>
2019-05-18 16:26:31 -0300

    inserindo useReactNative()

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,10 @@
-import Reactotron from 'reactotron-react-native';
-import { reactotronRedux } from 'reactotron-redux';
-import sagaPlugin from 'reactotron-redux-saga';
+import Reactotron from "reactotron-react-native";
+import { reactotronRedux } from "reactotron-redux";
+import sagaPlugin from "reactotron-redux-saga";
 
 if (__DEV__) {
   const tron = Reactotron.configure()
+    .useReactNative()
     .use(reactotronRedux())
     .use(sagaPlugin())
     .connect();
